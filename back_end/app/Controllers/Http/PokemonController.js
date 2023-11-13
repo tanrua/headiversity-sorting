8340601dc0af51073b569a944092c39947e42057
Simeon <simeon.gordon@fuseforward.com>
2023-11-13 13:59:25 -0800

    query params for the pokemon stats charts with... rudimentary validation

--- back_end/app/Controllers/Http/PokemonController.js
+++ back_end/app/Controllers/Http/PokemonController.js
@@ -1,6 +1,26 @@
 'use strict'
 
 const Pokemon = use('App/Models/Pokemon')
+const TYPE_ENUM = {
+  BUG: "BUG",
+  DARK: "DARK",
+  DRAGON: "DRAGON",
+  ELECTRIC: "ELECTRIC",
+  FAIRY: "FAIRY",
+  FIGHTING: "FIGHTING",
+  FIRE: "FIRE",
+  FLYING: "FLYING",
+  GHOST: "GHOST",
+  GRASS: "GRASS",
+  GROUND: "GROUND",
+  ICE: "ICE",
+  NORMAL: "NORMAL",
+  POISON: "POISON",
+  PSYCHIC: "PSYCHIC",
+  ROCK: "ROCK",
+  STEEL: "STEEL",
+  WATER: "WATER"
+}
 
 class PokemonController {
   async index ({ response }) {
@@ -21,13 +41,39 @@ class PokemonController {
   }
 
   async findChartable ({ request, response }) {
+    // This validation probably doesn't work doe to the adonis version problems discovered.
+
+    // const chartableSchema = Validator.schema.create({
+    //   type: Validator.schema.enum(Object.values(TYPE_ENUM)),
+    //   sorter: Validator.schema.string({ trim: true }),
+    //   count: Validator.schema.number(),
+    // })
+    // const payload = request.validate(chartableSchema)
+    
     const query_params = request.qs
-    const sorter = query_params.sorter
+
     const type = query_params.type
+    const sorter = query_params.sorter
     const count = query_params.count
 
-    const pokemon = await Pokemon.query().where('type', type).orderBy(sorter, 'desc').limit(count).fetch()
+    let pokemonQuery = Pokemon.query()
 
+    // We do not like this query string validation. No we do not! You can do better.... but it works given even bigger root problems
+    if (type){
+      pokemonQuery = pokemonQuery.where('type', type)
+    }
+
+    if (sorter){
+      pokemonQuery = pokemonQuery.orderBy(sorter, 'desc')
+    }
+
+    if (count){
+      pokemonQuery = pokemonQuery.limit(count)
+    } else {
+      pokemonQuery = pokemonQuery.limit(10)
+    }
+
+    const pokemon = await pokemonQuery.fetch()
     return response.status(200).json({ pokemon })
   }
 }

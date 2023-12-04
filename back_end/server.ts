8ca2b2997caf913554e639433c6d2d1a61613c84
Simeon <simeon.gordon@fuseforward.com>
2023-12-03 17:55:47 -0800

    adding a self-signed certificate only valid for localhost

--- back_end/server.ts
+++ back_end/server.ts
@@ -12,10 +12,22 @@
 
 import 'reflect-metadata'
 import sourceMapSupport from 'source-map-support'
+import { createServer } from 'https'
 import { Ignitor } from '@adonisjs/core/build/standalone'
+import { join } from 'path'
+import { readFileSync } from 'fs'
 
 sourceMapSupport.install({ handleUncaughtExceptions: false })
 
-new Ignitor(__dirname)
-  .httpServer()
-  .start()
+const privateKey = readFileSync(join(__dirname + '/config/sslCert/server.key'), 'utf8')
+const certificate = readFileSync(join(__dirname + '/config/sslCert/server.crt'), 'utf8')
+
+new Ignitor(__dirname).httpServer().start((handle) => {
+  return createServer(
+    {
+      key: privateKey,
+      cert: certificate,
+    },
+    handle
+  )
+}).catch(console.error)
